7d9daa6e818f47983d29169ab73f96969d4213de
Damely Tineo <damely.tineo07@gmail.com>
2020-01-18 06:57:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,14 +6,14 @@ class Dog
   
   def initialize(name)
     @name = name
-    save
+    save #method @@all << self -- here self refers to new instance we are storing in @@all class variable 
   end
   
-  def self.all
+  def self.all #class method serving as "reader" for @@all class variable. An attribute reader allows us to access variables in a class from outside that class. 
     @@all
   end
 
-  def self.print_all
+  def self.print_all #class method 
     @@all.each do |puppy|
       puts puppy.name
     end
